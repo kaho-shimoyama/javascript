0c6b6150ffed8efd35e279c5a5026a9d77b91bfd
EC2 Default User <ec2-user@ip-172-31-36-26.ec2.internal>
2022-04-23 13:35:24 +0000

    エラーとインデントの修正

--- main.js
+++ main.js
@@ -11,32 +11,31 @@
   let elapsedTime = 0;
   
   function setBtnStateInitial(){
-   start.disabled = false;
-   stop.disabled = true;
-   reset.disabled = true;
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = true;
   }
   
   function setBtnStateRunning(){
-   start.disabled = true;
-   stop.disabled = false;
-   reset.disabled = false;
+    start.disabled = true;
+    stop.disabled = false;
+    reset.disabled = false;
   }
   
   function setBtnStateStop(){
-   start.disabled = false;
-   stop.disabled =  true;
-   reset.disabled = false;
+    start.disabled = false;
+    stop.disabled =  true;
+    reset.disabled = false;
   }
   
-　function countUp() {
-  console.log(Date.now() - startTime);
-  const d = new Date(Date.now() - startTime + elapsedTime);
-　const m = String(d.getMinutes()).padStart(2, '0');
-　const s = String(d.getSeconds()).padStart(2, '0');
-　const ms = String(d.getMilliseconds()).padStart(3, '0');
-  display.textContent = `${m} : ${s} . ${ms}`;
-　clearId = setTimeout(() => {
-　countUp();
+  function countUp() {
+    const d = new Date(Date.now() - startTime + elapsedTime);
+　  const m = String(d.getMinutes()).padStart(2, '0');
+    const s = String(d.getSeconds()).padStart(2, '0');
+　  const ms = String(d.getMilliseconds()).padStart(3, '0');
+    display.textContent = `${m} : ${s} . ${ms}`;
+　  clearId = setTimeout(() => {
+　  countUp();
 　}, 10);
 　}
  
@@ -46,13 +45,13 @@
     startTime = Date.now();
     countUp();
     setBtnStateRunning();
-   });
+  });
 
   stop.addEventListener("click", function(){
     clearTimeout(clearId);
     setBtnStateStop();
     elapsedTime += Date.now() - startTime;
- 	 });
+   });
  	
   reset.addEventListener("click", function(){
     display.textContent = "００:００.０００";
