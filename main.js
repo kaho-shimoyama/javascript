fe22a2ec09e067f50f0391c80ba24c232a9c828c
EC2 Default User <ec2-user@ip-172-31-36-26.ec2.internal>
2022-04-23 02:18:02 +0000

    ストップウォッチの提出

---
+++ main.js
@@ -0,0 +1,61 @@
+'use strict';
+
+{ 
+  const display=document.getElementById("display");
+  const start= document.getElementById("start");
+  const stop= document.getElementById("stop");
+  const reset= document.getElementById("reset");
+  
+  let startTime ;
+  let clearId;
+  
+  function setBtnStateInitial(){
+   start.disabled = false;
+   stop.disabled = true;
+   reset.disabled = true;
+  }
+  
+  function setBtnStateRunning(){
+   start.disabled = true;
+   stop.disabled = false;
+   reset.disabled = false;
+  }
+  
+  function setBtnStateStop(){
+   start.disabled = false;
+   stop.disabled =  true;
+   reset.disabled = false;
+  }
+  
+　function countUp() {
+  console.log(Date.now() - startTime);
+  const d = new Date(Date.now() - startTime);
+　const m = String(d.getMinutes()).padStart(2, '0');
+　const s = String(d.getSeconds()).padStart(2, '0');
+　const ms = String(d.getMilliseconds()).padStart(3, '0');
+  display.textContent = `${m} : ${s} . ${ms}`;
+　clearId = setTimeout(() => {
+　countUp();
+　}, 10);
+　}
+ 
+  setBtnStateInitial();
+  
+  start.addEventListener("click", ()=>{
+    startTime = Date.now();
+    countUp();
+    setBtnStateRunning();
+   });
+
+  stop.addEventListener("click", function(){
+    clearTimeout(clearId);
+    setBtnStateStop();
+ 	 });
+ 	
+  reset.addEventListener("click", function(){
+    display.textContent = "００:００.０００";
+    setBtnStateInitial();
+   });
+
+}
+
